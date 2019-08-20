1663f4561f928e20d5a53c02e2f45b79193586be
Jiří Kiner <53608926+JiriKiner@users.noreply.github.com>
2019-08-20 14:57:12 +0200

    Add files via upload

---
+++ week2_capitalize1.c
@@ -0,0 +1,17 @@
+// Capitalizes string using ctype library (and an unnecessary condition)
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+	string s = get_string("Before: ");
+	printf("After: ");
+	for (int i = 0, n = strlen(s); i < n; i++)
+	{
+		printf("%c", toupper(s[i]));
+	}
+	printf("\n");
+}
