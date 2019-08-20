0da1bd3d0aef3f3b4a84cde174b15dd0b5e0b440
Jiří Kiner <53608926+JiriKiner@users.noreply.github.com>
2019-08-20 11:12:00 +0200

    Add files via upload

---
+++ week2_scores2.c
@@ -0,0 +1,34 @@
+// Generates a bar chart of three scores using an array
+
+#include <stdio.h>
+#include <cs50.h>
+
+const int COUNT = 3;
+
+void chart(int score);
+
+int main(void)
+{
+	// Get scores from user
+	int scores[COUNT];
+	for (int i = 0; i < COUNT; i++)
+	{
+		scores[i] = get_int("Score %i: ", i + 1);
+	}
+
+	// Chart scores
+	for (int i = 0; i < COUNT; i++)
+	{
+		printf("Score %i: ", i + 1);
+		chart(scores[i]);
+	}
+}
+
+void chart(int score)
+{
+	for (int i = 0; i < score; i++)
+	{
+		printf("#");
+	}
+	printf("\n");
+}
